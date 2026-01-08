c7c6aaebbf90840f57cf9d18c98910303ca80197
onursahin38 <sahinonur.158@gmail.com>
2026-01-09 00:21:52 +0300

    Update TestingScript.cs

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -1,8 +1,10 @@
 
 using UnityEngine;
+using System.Collections.Generic;
 
-public class TestingScript : MonoBehaviour
-
+namespace OnurNamespace
+{
+    
 {
 
   
@@ -11,17 +13,54 @@ public class TestingScript : MonoBehaviour
 
     int[] numbers = new int[3];
 
+    List<int> numbersList = new List <int>();
+
+        
+
     void Start()
 
     {
-        numbers[0] = 5;
-        numbers[1] = 8;
-        numbers[2] = 11;
+    //     numbers[0] = 5;
+    //     numbers[1] = 8;
+    //     numbers[2] = 11;
 
-        for( int i = 0;  i < numbers.Length; i++ )
+    //     for( int i = 0;  i < numbers.Length; i++ )
+    // {
+    //     Debug.Log(numbers[i]); 
+    // }
+
+    numbersList.Add(10);
+    numbersList.Add(15);
+    numbersList.Add(20);
+    Printlist();
+
+    numbersList.Remove(20);
+      Printlist();
+    numbersList.Clear();
+    for (int i=0; i < numbersList.Count; i++)
     {
-        Debug.Log(numbers[i]); 
+        Debug.Log(numbersList[i]);
+    }
+
+    void Printlist  ()
+    {
+        // for (int i=0; i< numbersList.Count; i++)
+        //         {   
+
+        //     Debug.Log (numbersList[i]);
+                    // }
+    foreach (int myNumber in numbersList)
+    {
+        Debug.Log(myNumber);
+    }
+    
+        
+    
+         Debug.Log ("--------------");
     }
 
 }
 }
+}
+
+public class TestingScript : MonoBehaviour
