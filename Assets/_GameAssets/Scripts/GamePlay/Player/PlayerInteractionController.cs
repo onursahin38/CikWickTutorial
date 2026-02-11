7bcea464a5d8a375da4307707216f50678547de2
onursahin38 <sahinonur.158@gmail.com>
2026-02-12 00:22:45 +0300

    v0.8 interface Added to Collectibles

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -6,20 +6,13 @@ public class PlayerInteractionController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag (Consts.WheatTypes.GOLD_WHEAT))
-        {
-            other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
-        }
 
-        if(other.CompareTag (Consts.WheatTypes.HOLY_WHEAT))
-        {   
-        other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
-        }
-
-        if(other.CompareTag (Consts.WheatTypes.ROTTEN_WHEAT))
+        if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
-       other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
-        }
+            collectible.Collect();
+        }      
+
+        
 
     }
 
@@ -29,6 +22,6 @@ public class PlayerInteractionController : MonoBehaviour
 
 
 
-
+    
 
 }
