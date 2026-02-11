99734eeb9f432f7074f1fcab3849e2e3c509fbb6
onursahin38 <sahinonur.158@gmail.com>
2026-02-11 23:23:01 +0300

    v0.7 Collectibles Added

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -39,17 +39,20 @@ private StateController _StateController;
 
 private Rigidbody _playerRigidbody;
 
+private float _startingMovementSpeed, _startingJumpForce;   
+
 private float _horizontalInput , _verticalInput;
 private Vector3 _movementDirection;
 private bool _isSliding;
     private int playerController_OnPlayerJumped;
 
-    void Awake()
+    private void Awake()
     {
         _StateController =  GetComponent <StateController>();
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
-
+        _startingMovementSpeed = _movementSpeed;
+        _startingJumpForce = _jumpForce;    
        
     }
 
@@ -187,6 +190,7 @@ private void SetPlayerMovement()
         _canJump = true;   
     }
 
+        #region Helpers Functions
     private bool IsGrounded()
     {
         
@@ -204,5 +208,29 @@ private void SetPlayerMovement()
         return _isSliding; 
     }
 
+    public void SetMovementSpeed (float speed, float duration)
+    {
+        _movementSpeed +=speed;
+        Invoke (nameof  (ResetMovementSpeed),duration);           
+    }
+
+    private void ResetMovementSpeed()
+    {
+        
+        _movementSpeed = _startingMovementSpeed;
+    }
+
+    public void SetJumpForce(float force, float duration)
+    {
+        _jumpForce += force;
+        Invoke(nameof (ResetJumpForce),duration);
+    }
+    private void ResetJumpForce()
+    {
+        
+        _jumpForce = _startingJumpForce;
+    }
+
+    #endregion
 
 }
