b17f0844f0ad796c2f7d17f92a6f9f4615b52080
onursahin38 <sahinonur.158@gmail.com>
2026-02-12 01:09:31 +0300

    v0.9 Scriptable Objects Implemented

---
+++ Assets/_GameAssets/Scripts/ScriptableObjects/WheatDesignSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="WheatDesignSO" , menuName = "ScriptableObjects/WheatDesignSO")]
+public class WheatDesignSO : ScriptableObject
+{
+    [SerializeField] private float _increaseDecreaseMultiplier;
+    [SerializeField] private float _resetBoostDuration;
+
+    public float IncraseDecreaseMultiplier => _increaseDecreaseMultiplier;
+    public float ResetBoostDuration => _resetBoostDuration;
+
+
+}
